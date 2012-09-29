fc83afa4a6af110e51c2e7c9d5ff72f61d011492
Quinn Slack <qslack@qslack.com>
2012-09-29 04:37:50 -0700

    factor out HttpClientConnection onResponse handler

--- lib/resource.dart
+++ lib/resource.dart
@@ -1,3 +1,5 @@
+typedef void HttpClientResponseCallback(HttpClientResponse response);
+
 class Resource {
   String url;
 
@@ -10,23 +12,17 @@ class Resource {
 
   void query(Map<String, String> params, void onSuccess(Object data)) {
     HttpClientConnection clientConn = httpClient.get('localhost', 9000, url);
-    clientConn.onResponse = (HttpClientResponse response) {
-      StringInputStream stream = new StringInputStream(response.inputStream);
-      StringBuffer body = new StringBuffer();
-      stream.onData = () {
-        body.add(stream.read());
-      };
-      stream.onClosed = () {
-        Object data = JSON.parse(body.toString());
-        onSuccess(data);
-      };
-    };
+    clientConn.onResponse = _makeResponseHandler(onSuccess);
   }
 
   void get(Map<String, String> params, void onSuccess(Object data)) {
     String id = params['id'];
     HttpClientConnection clientConn = httpClient.get('localhost', 9000, '${url}/${id}');
-    clientConn.onResponse = (HttpClientResponse response) {
+    clientConn.onResponse = _makeResponseHandler(onSuccess);
+  }
+
+  HttpClientResponseCallback _makeResponseHandler(void onSuccess(Object data)) {
+    return (HttpClientResponse response) {
       StringInputStream stream = new StringInputStream(response.inputStream);
       StringBuffer body = new StringBuffer();
       stream.onData = () {
