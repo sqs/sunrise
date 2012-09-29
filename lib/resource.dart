975e1b9a788be7c7b08585c85f533a30c028bff6
Quinn Slack <qslack@qslack.com>
2012-09-29 04:26:43 -0700

    whitespace

--- lib/resource.dart
+++ lib/resource.dart
@@ -1,41 +1,41 @@
 class Resource {
-    String url;
+  String url;
 
-    final HttpClient httpClient;
+  final HttpClient httpClient;
 
-    Resource(String url, [HttpClient httpClient = null])
-    : url = url,
-      httpClient = ((httpClient != null) ? httpClient : new HttpClient()) {
-    }
+  Resource(String url, [HttpClient httpClient = null])
+  : url = url,
+    httpClient = ((httpClient != null) ? httpClient : new HttpClient()) {
+  }
 
-    void query(Map<String, String> params, void onSuccess(Object data)) {
-        HttpClientConnection clientConn = httpClient.get('localhost', 9000, url);
-        clientConn.onResponse = (HttpClientResponse response) {
-            StringInputStream stream = new StringInputStream(response.inputStream);
-            StringBuffer body = new StringBuffer();
-            stream.onData = () {
-                body.add(stream.read());
-            };
-            stream.onClosed = () {
-              Object data = JSON.parse(body.toString());
-              onSuccess(data);                
-            };
-        };
-    }
+  void query(Map<String, String> params, void onSuccess(Object data)) {
+    HttpClientConnection clientConn = httpClient.get('localhost', 9000, url);
+    clientConn.onResponse = (HttpClientResponse response) {
+      StringInputStream stream = new StringInputStream(response.inputStream);
+      StringBuffer body = new StringBuffer();
+      stream.onData = () {
+        body.add(stream.read());
+      };
+      stream.onClosed = () {
+        Object data = JSON.parse(body.toString());
+        onSuccess(data);
+      };
+    };
+  }
 
-    void get(Map<String, String> params, void onSuccess(Object data)) {
-        String id = params['id'];
-        HttpClientConnection clientConn = httpClient.get('localhost', 9000, '${url}/${id}');
-        clientConn.onResponse = (HttpClientResponse response) {
-            StringInputStream stream = new StringInputStream(response.inputStream);
-            StringBuffer body = new StringBuffer();
-            stream.onData = () {
-                body.add(stream.read());
-            };
-            stream.onClosed = () {
-              Object data = JSON.parse(body.toString());
-              onSuccess(data);                
-            };
-        };
-    }
+  void get(Map<String, String> params, void onSuccess(Object data)) {
+    String id = params['id'];
+    HttpClientConnection clientConn = httpClient.get('localhost', 9000, '${url}/${id}');
+    clientConn.onResponse = (HttpClientResponse response) {
+      StringInputStream stream = new StringInputStream(response.inputStream);
+      StringBuffer body = new StringBuffer();
+      stream.onData = () {
+        body.add(stream.read());
+      };
+      stream.onClosed = () {
+        Object data = JSON.parse(body.toString());
+        onSuccess(data);
+      };
+    };
+  }
 }
