0d54a6b08ddc2a0e5cd2d7a7edc1253df878c91b
Quinn Slack <qslack@qslack.com>
2012-09-30 18:48:47 -0700

    slider demo: add SliderCtrl controller

--- samples/slider/slider.dart
+++ samples/slider/slider.dart
@@ -1,3 +1,13 @@
 #library("slider");
 
-#import("../../lib/sunrise.dart");
+#import("../../lib/sunrise.dart", prefix: 'sunrise');
+
+class SliderCtrl extends sunrise.Controller {
+  void init() {
+    scope['currentDate'] = (new Date.now()).toString();
+  }
+}
+
+main() {
+  sunrise.main();
+}
