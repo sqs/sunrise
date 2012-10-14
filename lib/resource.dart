fcb969dc3a4b06b9475d0f3b40811d85ce283935
Quinn Slack <qslack@qslack.com>
2012-10-13 17:22:25 -0700

    ResourceCollection<T> implementing Collection<T> with .length and .iterator()

--- lib/resource.dart
+++ lib/resource.dart
@@ -1,14 +1,17 @@
 typedef HttpRequest HttpRequestFactory();
 typedef void HttpClientResponseCallback(HttpRequest request);
 
-class Resource {
+class Resource<T> {
   String url;
+
+  ResourceCollection<T> collection;
 
   final HttpRequestFactory httpRequestFactory;
 
   Resource(String url, [HttpRequestFactory httpRequestFactory = null])
   : url = url,
     httpRequestFactory = (httpRequestFactory != null) ? httpRequestFactory : (() => new HttpRequest()) {
+    collection = new ResourceCollection<T>(this);
   }
 
   void query(Map<String, String> params, void onSuccess(Object data)) {
@@ -38,5 +41,47 @@ class Resource {
     request.on.error.add((event) {
       window.console.error('HttpRequest error: ${event}');
     });
+  }
+}
+
+typedef void OnLoadFn(ResourceCollection<T> rc);
+
+class ResourceCollection<T> implements Collection<T> {
+  final Resource<T> resource;
+  bool loaded = false;
+
+  OnLoadFn _onLoadFn = null;
+  Collection<T> _collection = [];
+
+  ResourceCollection(this.resource);
+
+  void onLoad(void onLoadFn(ResourceCollection<T> rc)) {
+    _onLoadFn = onLoadFn;
+  }
+
+  void _ensureLoadStarted() {
+    if (loaded == false) {
+      _load();
+    }
+  }
+
+  void _load() {
+    resource.query({}, (data) {
+      _collection = data;
+      loaded = true;
+      if (_onLoadFn != null) {
+        _onLoadFn(this);
+      }
+    });
+  }
+
+  int get length {
+    _ensureLoadStarted();
+    return _collection.length;
+  }
+
+  Iterator<T> iterator() {
+    _ensureLoadStarted();
+    return _collection.iterator();
   }
 }
